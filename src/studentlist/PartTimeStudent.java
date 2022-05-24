6d0d652f15591d8fb0abe872e895376af91ef4f7
joshua abeto <abetojo@sheridancollege.ca>
2022-05-24 14:07:18 -0400

    add part time branch

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,19 @@
+
+package studentlist;
+
+/**
+ * SYST17796 Fundamentals of Software Design and Development
+ * @author Joshua Abeto
+ */
+public class PartTimeStudent extends Student {
+    private int numOfCourses;
+
+    
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+} //end of class
